3b58ed047cc3e605a47a3ba529b24fbbe8c67c79
Sakti772589 <SB00502836@techmahindra.com>
2020-03-20 20:14:28 +0530

    class component

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import { Layout, Menu, Breadcrumb } from "antd";
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
-
-
 const options = {
   title: {
     text: "My chart"
@@ -20,55 +18,70 @@ const options = {
   ]
 };
 
-const App = () => {
-  return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Sider collapsible>
-        <div className="logo" />
-        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-          <Menu.Item key="1">
-            <span>Option 1</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <span>Option 2</span>
-          </Menu.Item>
-          <SubMenu
-            key="sub1"
-            title={
-              <span>
-                <span>User</span>
-              </span>
-            }
-          >
-            <Menu.Item key="3">Tom</Menu.Item>
-            <Menu.Item key="4">Bill</Menu.Item>
-            <Menu.Item key="5">Alex</Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="sub2"
-            title={
-              <span>
-                <span>Team</span>
-              </span>
-            }
-          >
-            <Menu.Item key="6">Team 1</Menu.Item>
-            <Menu.Item key="8">Team 2</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="9"></Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }} />
-        <Content style={{ margin: "0 16px" }}>
-          <HighchartsReact highcharts={Highcharts} options={options} />
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
+class App extends Component<{}> {
+  chartComponent: any;
+  constructor(props: {}) {
+    super(props);
+    this.chartComponent = React.createRef();
+  }
+
+  componentDidMount() {
+    this.chartComponent.current.chart.reflow();
+  }
+  render() {
+    return (
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider collapsible>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1">
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <span>User</span>
+                </span>
+              }
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <span>Team</span>
+                </span>
+              }
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9"></Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: "0 16px" }}>
+            <HighchartsReact
+              highcharts={Highcharts}
+              ref={this.chartComponent}
+              options={options}
+            />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
-  );
-};
+    );
+  }
+}
 
 export default App;
